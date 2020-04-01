8c03f6ffd6b92b73ad83ecaa7f35d28c45b5d118
Gregory Durelle <gdurelle@protonmail.com>
2020-04-01 23:07:22 +0200

    Resuming work
    add zip extraction gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Starting extraction for zipped CSV file...'
+
+require 'zip'
+file_path = 'db/memory-tech-challenge-data.csv.zip'
+zip_file = Zip::File.open(file_path)
+entry = zip_file.glob('*.csv').first
+csv_text = entry.get_input_stream.read
+
+puts 'CSV has been extracted.'
+
+puts 'starting DB import'
+# csv = CSV.parse(csv_text, :headers=>true)
